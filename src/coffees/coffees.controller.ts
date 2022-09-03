878b0196f0480deb0fb9bc80be42c9c5f90af914
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-03 11:48:33 +0200

    Response status codes

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { get } from 'http';
+
+@Controller('coffees')
+export class CoffeesController {
+    // @Get('flavors')
+    // findAll() {
+    //     return 'this action returns all coffees'
+    // }
+
+    @Get()
+    findAll(@Res() response) {
+        response.status(200).send('this action returns all coffees')
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string)
+    {
+        return `This action returns #${id} coffee`
+    }
+
+
+    @Post()
+    @HttpCode(HttpStatus.GONE)
+    create(@Body() body)
+    {
+        return body
+    }
+
+    // @Post()
+    // create(@Body('name') body)
+    // {
+    //     return body
+    // }
+
+
+}
