ab2e81ab0158ed1b95693c1f1671cb1ea63f1e62
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-17 21:34:24 +0200

    Non-class-based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,11 +5,20 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+
+export class MockCoffeesService { }
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS, // 👈
+      useValue: ['buddy brew', 'nescafe']
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule { }
