9906aab6cf436f70f3d85c9bfb7716eeb99da5df
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-17 21:22:56 +0200

    Finished Control NestJS Module Encapsulation

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,10 +1,9 @@
-import { CoffeesService } from './../coffees/coffees.service';
 import { CoffeesModule } from './../coffees/coffees.module';
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
-  imports: [CoffeesModule],  
-  providers: [CoffeeRatingService],
+  imports: [CoffeesModule],
+  providers: [CoffeeRatingService]
 })
 export class CoffeeRatingModule {}
