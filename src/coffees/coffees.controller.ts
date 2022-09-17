d2bf74f2943045d8b2216a33f0791ccf9418b8e9
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-17 17:31:05 +0200

    chapter 2 done

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,39 +1,97 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
-import { get } from 'http';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Patch,
+    Post,
+    Res,
+    Query
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
-
+    constructor(private readonly coffeesService: CoffeesService) {}
 
     @Get()
-    findAll() {
-        return 'this action returns all coffees'
+    findAll(@Query() paginationQuery) {
+        // const { limit, offset } = paginationQuery; 
+        return this.coffeesService.findAll();
     }
+
+    // @Get()
+    // findAll(@Query() paginationQuery) {
+    //     const { limit, offset } = paginationQuery; 
+    //     return `this action returns all coffees. Limit: ${limit}, offset: ${offset}`
+    // }
 
     @Get(':id')
-    findOne(@Param('id') id: string)
-    {
-        return `This action returns #${id} coffee`
+    findOne(@Param('id') id: number) {
+        console.log(typeof id)
+        return this.coffeesService.findOne(''+id);
+    }
+    
+    // @Get(':id')
+    // findOne(@Param('id') id: string)
+    // {
+    //     return `This action returns #${id} coffee`
+    // }
+
+     @Post()
+    create(@Body() createCoffeeDto: CreateCoffeeDto) 
+     {
+        console.log(createCoffeeDto instanceof CreateCoffeeDto)
+        return this.coffeesService.create(createCoffeeDto);
     }
 
+    // @Post()
+    // create(@Body() body) 
+    // {
+    //     return this.coffeesService.create(body);
+    // }
 
-    @Post()
-    create(@Body() body)
-    {
-        return body
-    }
+    // @Post()
+    // create(@Body() body) 
+    // {
+    //     return body
+    // }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body)
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto)
     {
-        return `This actions updates #${id} coffee`
+        return this.coffeesService.update(id, updateCoffeeDto);
     }
+    
+    // @Patch(':id')
+    // update(@Param('id') id: string, @Body() body)
+    // {
+    //     return this.coffeesService.update(id, body);
+    // }
+    
+    // @Patch(':id')
+    // update(@Param('id') id: string, @Body() body)
+    // {
+    //     return `This actions updates #${id} coffee`
+    // }
+
 
     @Delete(':id')
     remove(@Param('id') id: string)
     {
-        return `This actions removes #${id} coffee`
+        return this.coffeesService.remove(id);
     }
+
+    // @Delete(':id')
+    // remove(@Param('id') id: string)
+    // {
+    //     return `This actions removes #${id} coffee`
+    // }
 
 
     // // @Get('flavors')
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    forbidNonWhitelisted: true,
+    transform: true, // to map body payloads to DTO automatically
+   whitelist: true,
+}));
   await app.listen(3000);
 }
 bootstrap();
