aefef72a99e78ebfd020cfc1294431977b2a728b
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-17 21:50:24 +0200

    Leverage Async Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,6 +6,7 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
 export class MockCoffeesService { }
 
@@ -26,18 +27,15 @@ export class CoffeeBrandsFactory {
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
-    {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
-    },
     {
       provide: COFFEE_BRANDS, // 👈
-      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+        useFactory: async (connection: Connection): Promise<string[]> => {
+    // const coffeeBrands = await connection.query('SELECT * ...');
+          const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe'])
+          console.log('[!] Async factory');
+    return coffeeBrands;
+  },
+  inject: [Connection],
     },
   ],
   exports: [CoffeesService],
